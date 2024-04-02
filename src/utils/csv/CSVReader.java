d42329d56cd261f0ff0e18c858a65d8b4d4e968a
Lucas Melo <casmelo.ms@gmail.com>
2024-04-01 23:40:15 -0300

    CSV reader and writer created

---
+++ src/utils/csv/CSVReader.java
@@ -0,0 +1,45 @@
+package utils.csv;
+
+import models.Task;
+import utils.enums.TaskPriority;
+import utils.enums.TaskStatus;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class CSVReader {
+
+    private static final String FILE_PATH = "tasks.csv";
+
+    public static List<Task> readTasks() {
+        List<Task> tasks = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        try {
+            tasks = Files.lines(Paths.get(FILE_PATH))
+                    .skip(1)
+                    .map(line -> line.split(","))
+                    .map(data -> new Task(
+                            UUID.fromString(data[0].trim()),
+                            data[1].trim(),
+                            data[2].trim(),
+                            LocalDateTime.parse(data[3].trim(), formatter),
+                            TaskPriority.valueOf(data[4].trim().toUpperCase()),
+                            TaskStatus.valueOf(data[5].trim().toUpperCase()),
+                            List.of(data[6].split(";"))
+                    ))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tasks;
+    }
+}
--- src/utils/csv/CSVWriter.java
+++ src/utils/csv/CSVWriter.java
@@ -2,11 +2,38 @@ package utils.csv;
 
 import models.Task;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
-public class TaskToCSVLine {
+public class CSVWriter {
 
-    public static String taskToCSVLine(Task task) {
+    private static final String FILE_PATH = "tasks.csv";
+
+    public static void writeTasks(List<Task> tasks) {
+        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(FILE_PATH))) {
+            tasks.stream()
+                    .map(CSVWriter::taskToCSVLine)
+                    .forEach(line -> {
+                        try {
+                            bw.write(line);
+                            bw.newLine();
+                        } catch (IOException e) {
+                            throw new UncheckedIOException(e);
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UncheckedIOException ue) {
+            System.out.println("Failed to write file: " + ue.getCause().getMessage());
+        }
+    }
+
+    private static String taskToCSVLine(Task task) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String expirationDateFormatted = task.expirationDate.format(formatter);
         String tags = String.join(";", task.tags);
