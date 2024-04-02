3497fe1b14ceed3d814a1269ecc3af1feaf38f89
Lucas Melo <casmelo.ms@gmail.com>
2024-04-01 21:29:20 -0300

    creating taskToCsv class method

--- src/utils/csv/TaskToCSVLine.java
+++ src/utils/csv/TaskToCSVLine.java
@@ -1,4 +1,26 @@
 package utils.csv;
 
-public class TaskToCSVLine(Task task) {
+import models.Task;
+
+import java.time.format.DateTimeFormatter;
+
+public class TaskToCSVLine {
+
+    public static String taskToCSVLine(Task task) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        String expirationDateFormatted = task.expirationDate.format(formatter);
+        String tags = String.join(";", task.tags); // Usamos ponto e vírgula para separar as tags
+
+        return String.join(",",
+                String.valueOf(task.id),
+                task.title.replace(",", ";"), // Substituir vírgulas para evitar conflitos no CSV
+                task.description.replace(",", ";"),
+                expirationDateFormatted,
+                task.priority.toString(),
+                task.status.toString(),
+                "\"" + tags + "\"" // Aspas para lidar com múltiplos valores
+        );
+
+    }
+
 }
